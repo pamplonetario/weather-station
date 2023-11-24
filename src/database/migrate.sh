70a9197291aee64008b244d15ff2cb9bb6c853c3
Carlos Fdez. Llamas <hello@sirikon.me>
2023-11-24 20:51:35 +0100

    Adjustments

--- src/database/migrate.sh
+++ src/database/migrate.sh
@@ -2,4 +2,7 @@
 set -euo pipefail
 
 export DATABASE_URL="mysql://${METEO_DB_USERNAME}:${METEO_DB_PASSWORD}@${METEO_DB_HOST}:3306/${METEO_DB_NAME}"
-dbmate --wait up
+export DBMATE_MIGRATIONS_DIR="./migrations"
+
+dbmate --wait migrate
+echo "Migration finished"
