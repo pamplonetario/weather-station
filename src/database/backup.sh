698ac80fd9708db268bdaf6d4b65e23cc08b247b
Carlos Fdez. Llamas <hello@sirikon.me>
2023-11-26 13:38:31 +0100

    Don't block tables while backing up

--- src/database/backup.sh
+++ src/database/backup.sh
@@ -7,7 +7,11 @@ mkdir -p /backups
 mysqldump \
   --user=root \
   --password="${MYSQL_ROOT_PASSWORD}" \
+  --single-transaction=TRUE \
   --no-create-db \
   --no-create-info \
   "${MYSQL_DATABASE}" |
   gzip >"/backups/${backup_name}.gz"
+
+# About --single-transaction
+# https://dev.mysql.com/doc/refman/5.7/en/mysqldump.html#option_mysqldump_single-transaction
