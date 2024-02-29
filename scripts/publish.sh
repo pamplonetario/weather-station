3c1adc682af46fde2e17832b4321946d98583cd9
Carlos Fdez. Llamas <hello@sirikon.me>
2024-02-29 21:46:09 +0100

    utc

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-WEATHER_STATION_VERSION="$(date '+%Y%m%d_%H%M%S')"
+WEATHER_STATION_VERSION="$(date -u '+%Y%m%d_%H%M%S')"
 WEATHER_STATION_TAG="ghcr.io/pamplonetario/weather-station:${WEATHER_STATION_VERSION}"
 docker build -t "$WEATHER_STATION_TAG" .
 docker push "$WEATHER_STATION_TAG"
